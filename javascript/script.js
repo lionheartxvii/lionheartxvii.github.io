da156f4304612982896b118ca3123f616359759c
lionheartxvii <71563922+lionheartxvii@users.noreply.github.com>
2023-03-30 15:26:56 +0200

    huge update

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,41 @@
+// Locale storage
+
+const listOfWomen = [
+  "fassbinder",
+  "leyen", 
+  "marvingt", 
+  "mayrisch",
+  "waha"
+];
+
+function checkLocaleStorage(){
+  if(!localStorage.getItem("herHistoryLocale")){
+    initLocaleStorage();
+  } else{
+    return;
+  }
+}
+
+function initLocaleStorage(){
+  var key;
+
+  listOfWomen.forEach(element => {
+    if(!localStorage.getItem(element)){
+      localStorage.setItem(element, false)
+    }
+
+    for(var i = 1; i <= 6; i++){
+      key = element + i.toString();
+      if (!localStorage.getItem(key) || localStorage.getItem(key) == null) {
+          localStorage.setItem(key, false)
+      }
+    }
+  })
+
+  localStorage.setItem("herHistoryLocale", true);
+}
+
+checkLocaleStorage();
 
 // Hamburger Menu
 
@@ -5,17 +43,29 @@ const menu_btn = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.mobile-nav');
 
 menu_btn.addEventListener('click', function () {
-  menu_btn.classList.toggle('is-active');
-  mobile_menu.classList.toggle('is-active');
+  if(chevron_btn.classList[1]){
+    dropdownLanguage();
+  }
+  dropdownMenu();
 });
 
 const chevron_btn = document.querySelector('.chevron');
 const lang_menu = document.querySelector('.mobile-languages');
 
 chevron_btn.addEventListener('click', function () {
-  chevron_btn.classList.toggle('is-active');
-  lang_menu.classList.toggle('is-active');
+  if(menu_btn.classList[1]){
+    dropdownMenu();
+  }
+  dropdownLanguage();
 });
+
+lang_menu.addEventListener('click', function () {
+  if(menu_btn.classList[1]){
+    dropdownMenu();
+  }
+  dropdownLanguage();
+});
+
 
 var animation1 = anime({
   targets: '.menu-item',
@@ -39,28 +89,108 @@ var animation2 = anime({
 });
 document.querySelector('.chevron').onclick = animation2.play;
 
-
-// Open Articles + Blinken
-let selector = document.getElementsByClassName('indicator');
-const listOfIndicators = [];
-for (i = 0; i < selector.length; i++) {
-  listOfIndicators[i] = selector[i].id;
+function dropdownLanguage(){
+  chevron_btn.classList.toggle('is-active');
+  lang_menu.classList.toggle('is-active');
 }
-console.log(listOfIndicators);
 
-let selector2 = document.getElementsByClassName('articles');
-const listOfArticles = [];
-for (i = 0; i < selector2.length; i++) {
-  listOfArticles[i] = selector2[i].id;
+function dropdownMenu(){
+  menu_btn.classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active');
 }
-console.log(listOfArticles);
 
+
+////////////////
+// Indicators //
+////////////////
+
+var uriKey = getUriKey();
 var currentNext = "";
+var listOfIndicators = initIndicators(uriKey);
+var listOfArticles = initArticle(uriKey);
 var indicatorAnimationBlink;
 
+// get Last part of the uri, without suffix 
+function getLastUriPart(){
+  const path = window.location.pathname;
+  const parts = path.split('/');
+  const lastPart = parts[parts.length - 1];
+  return variable = lastPart.split('.')[0];
+}
+
+// check and get of a key Value for the locale Storage
+// if the last uri part is a element of the listOfWomen
+function getUriKey(){
+  var uriPart = getLastUriPart();
+  if(uriPart === "index"){
+    checkPuzzle();
+  }
+  let match = listOfWomen.filter(element => element === uriPart);
+  if (match.length == 1){
+    return match[0];
+  }
+}
+
+
+// return a list of not triggerd Indicators
+function initIndicators(key){
+  if(!key) return;
+
+  let selector = document.getElementsByClassName('indicator');
+  let listOfIndicators = [];
+  for (i = 0; i < selector.length; i++) {
+    if(localStorage.getItem(uriKey + (i+1).toString()) === "false"){
+      listOfIndicators[i] = selector[i].id;
+    } else {
+      document.getElementById("ind-" + (i+1).toString()).style.borderColor = "#ebdc89";
+      document.getElementById("ind-" + (i+1).toString()).getElementsByClassName("text")[0].style.color = "#ebdc89";
+    }
+  }
+
+  listOfIndicators = listOfIndicators.filter(value => value !== undefined);
+  calcucatePositions(key);
+  return listOfIndicators;
+}
+
+function setPuzzleToStorage(){
+  let key = uriKey + "Puzzle";
+  localStorage.setItem(key, true);
+  document.getElementsByClassName("puzzle-piece")[0].style.cssText = "z-index: 300; opacity: 1";
+}
+
+function checkPuzzle(){
+  let key;
+  var collectedPuzzles = [];
+  listOfWomen.forEach(element => {
+    key = element + "Puzzle";
+    if(localStorage.getItem(key)){
+      setPuzzleToPicture(key);
+      collectedPuzzles.push(element);
+    }
+  })
+
+  if (collectedPuzzles.length === listOfWomen.length){
+    document.getElementById('particles-konfetti').style.opacity = 1;
+  }
+}
+
+function setPuzzleToPicture(key){
+  document.getElementById(key).classList.add("fill");
+}
+
+function initArticle(key){
+  if(!key) return;
+
+  let selector2 = document.getElementsByClassName('articles');
+  const listOfArticles = [];
+  for (i = 0; i < selector2.length; i++) {
+    listOfArticles[i] = selector2[i].id;
+  }
+  return listOfArticles;
+}
 
 async function setNextBlink() {
-
+  console.log(listOfIndicators);
   if (currentNext !== "") {
     indicatorAnimationBlink.duration = 100;
     indicatorAnimation.autoplay = false;
@@ -86,17 +216,22 @@ async function setNextBlink() {
       loop: true,
     });
     indicatorAnimationBlink.play();
+  } else if ( localStorage.getItem(uriKey + "Puzzle") !== "true"){
+    setPuzzleToStorage();
   }
-  console.log(currentNext);
+  console.log(listOfIndicators);
 }
 
 function func(id) {
-  document.getElementById("ind-" + id.toString()).style.borderColor = "#ebdc89";
-  document.getElementById("ind-" + id.toString()).getElementsByClassName("text")[0].style.color = "#ebdc89";
-  listOfIndicators.splice(listOfIndicators.indexOf("ind-" + id.toString()), 1);
-  setNextBlink();
+  console.log(localStorage.getItem(uriKey + id.toString()));
+  if (localStorage.getItem(uriKey + id.toString()) !== "true"){
+    document.getElementById("ind-" + id.toString()).style.borderColor = "#ebdc89";
+    document.getElementById("ind-" + id.toString()).getElementsByClassName("text")[0].style.color = "#ebdc89";
+    localStorage.setItem(uriKey + id.toString(), true);
+    listOfIndicators.splice(listOfIndicators.indexOf("ind-" + id.toString()), 1);
+    setNextBlink();
+  }
   document.getElementById("article-container-" + id.toString()).style.height = "100vh"; // Texte einblenden 
-
 }
 
 // Texte ausblenden
@@ -108,14 +243,28 @@ function closeArticle(id) {
 function getGallery(id) {
   document.getElementById("ind-" + id.toString()).style.borderColor = "#ebdc89";
   document.getElementById("ind-" + id.toString()).getElementsByClassName("text")[0].style.color = "#ebdc89";
-  listOfIndicators.splice(listOfIndicators.indexOf("ind-" + id.toString()), 1);
-  setNextBlink();
-  document.getElementById("picture-gallery").style.cssText = "z-index: 500; opacity: 1";
+  document.getElementById("picture-gallery").style.cssText = "z-index: 400; opacity: 1";
 }
 
 // Gallerie ausblenden
 function closeGallery(id) {
   document.getElementById("picture-gallery").style.cssText = "z-index: -10; opacity: 0";
+}
+
+// Puzzle einblenden
+function openPuzzleScreen(id) {
+  shakeanimation.pause(); 
+  // document.getElementsByClassName("puzzle-piece")[0].style.cssText = "cursor: none";
+  var listHiddenElements = document.getElementsByClassName("hide");
+  for (var i = 0; i < listHiddenElements.length; i++){
+    listHiddenElements[i].style.cssText = "z-index: 300 ; opacity: 1";
+  }
+  document.getElementsByClassName("puzzle-piece")[0].style.cssText = "cursor: default; z-index: 300";
+}
+
+// Puzzle ausblenden
+function closePuzzleScreen(id) {
+  document.getElementById("puzzleScreen").style.cssText = "z-index: -10 ; opacity: 0";
 }
 
 // Click Indicator
@@ -260,3 +409,146 @@ function hideInfo(infoBox) {
 }
 
 
+// End Screen
+// Click Indicator
+
+
+const xMax = 6;
+var shakeanimation = anime({
+  targets: '.map',
+  easing: 'easeInOutSine',
+  duration: 600,
+  loop: true,
+  delay: 1000,
+  rotate: [
+    {
+      value: xMax * -1,
+    },
+    {
+      value: xMax,
+    },
+    {
+      value: xMax/-2,
+    },
+    {
+      value: xMax/2,
+    },
+    {
+      value: 0
+    }
+  ],
+});
+
+
+function getIndicatorPos(key){
+  var IndicatorsFassbinder = [
+    { x: 0.58, y: 0.51 },
+    { x: 0.88, y: 0.18 },
+    { x: 0.15, y: 0.42 },
+    { x: 0.66, y: 0.28 },
+    { x: 0.48, y: 0.62 },
+    { x: 0.36, y: 0.34 },
+  ];
+  
+  var IndicatorsLeyen = [
+    { x: 0.265, y: 0.21 },
+    { x: 0.76, y: 0.18 },
+    { x: 0.84, y: 0.73 },
+    { x: 0.28, y: 0.51 },
+    { x: 0.545, y: 0.28 },
+    { x: 0.3, y: 0.72 },
+  ];
+  
+  var IndicatorsMarvingt = [
+    { x: 0.26, y: 0.26 },
+    { x: 0.13, y: 0.53 },
+    { x: 0.73, y: 0.58 },
+    { x: 0.39, y: 0.6 },
+    { x: 0.49, y: 0.89 },
+    { x: 0.33, y: 0.49 },
+  ];
+  
+  var IndicatorsMayrisch = [
+    { x: 0.5, y: 0.28 },
+    { x: 0.89, y: 0.69 },
+    { x: 0.67, y: 0.66 },
+    { x: 0.91, y: 0.23 },
+    { x: 0.20, y: 0.6 },
+    { x: 0.46, y: 0.64 },
+  ];
+  
+  var IndicatorsWaha = [
+    { x: 0.20, y: 0.30 },
+    { x: 0.84, y: 0.64 },
+    { x: 0.1, y: 0.85 },
+    { x: 0.84, y: 0.26 },
+    { x: 0.57, y: 0.61 },
+    { x: 0.38, y: 0.72 },
+  ];
+
+  switch(key){
+    case 'fassbinder':
+      return IndicatorsFassbinder;
+    case 'leyen':
+      return IndicatorsLeyen;
+    case 'marvingt':
+      return IndicatorsMarvingt;
+    case 'mayrisch':
+      return IndicatorsMayrisch;
+    case 'waha':
+      return IndicatorsWaha;
+    default:
+      return null;
+  }
+}
+
+function calcucatePositions(){
+  var canvas = document.getElementById("backgroundImage");
+  var indicators = document.getElementsByClassName("indicator");
+  var position = getIndicatorPos(uriKey);
+
+  if (position === null){
+    return;
+  }
+
+  // console.log(canvas.width);
+  // console.log(canvas.height);
+  for (var i = 0; i < 6; i++){
+    indicators[i].style.left = `${canvas.offsetLeft + canvas.width * position[i].x}px`;
+    indicators[i].style.top = `${canvas.offsetTop + canvas.height * position[i].y}px`;
+  }
+}
+
+window.addEventListener('resize', () => {
+  calcucatePositions();
+  // calculateCropping();
+});
+
+window.onload = (() => {
+  calcucatePositions();
+  // calculateCropping();
+});
+
+// function calculateCropping() {
+//   var container = document.getElementsByClassName("room-container")[0];
+//   var img = document.getElementById("backgroundImage");
+
+//   const containerWidth = container.offsetWidth;
+//   const containerHeight = container.offsetHeight;
+//   const containerAspectRatio = containerWidth / containerHeight;
+//   const imgAspectRatio = img.width / img.height;
+
+//   let croppedWidth = 0;
+//   let croppedHeight = 0;
+
+//   if (containerAspectRatio > imgAspectRatio) {
+//       const scaledHeight = containerWidth / imgAspectRatio;
+//       croppedHeight = containerHeight - scaledHeight;
+//   } else {
+//       const scaledWidth = containerHeight * imgAspectRatio;
+//       croppedWidth = containerWidth - scaledWidth;
+//   }
+
+//   console.log(`Cropped width: ${croppedWidth}px`);
+//   console.log(`Cropped height: ${croppedHeight}px`);
+// }
