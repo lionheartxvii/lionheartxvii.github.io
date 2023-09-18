5677ed4ec868b37817d71ed2e5a01dee47443c7e
lionheartxvii <71563922+lionheartxvii@users.noreply.github.com>
2023-09-18 17:03:04 +0200

    more animations

--- javascript/animations.js
+++ javascript/animations.js
@@ -27,3 +27,40 @@ var IndicatorRemoval2 = new ScrollMagic.Scene({
     name: 'SCROLL2&3'
 })
 .setClassToggle('.text-indicator', 'active');
+
+// Lottie Animations
+
+LottieInteractivity.create({
+    mode: 'scroll',
+    player: '#illustration-1',
+    actions: [
+      {
+        visibility: [0.2, 1],
+        type: 'seek',
+        frames: [0, 110],
+      },
+      
+    ],
+
+    // container: "#section2",
+    // mode: 'cursor',
+    // actions: [
+    //   {
+    //       type: "hover",
+    //       forceFlag: false
+    //   }
+    // ],
+  });
+
+  LottieInteractivity.create({
+    mode: 'scroll',
+    player: '#illustration-2',
+    actions: [
+      {
+        visibility: [0.3, 1],
+        type: 'seek',
+        frames: [0, 220],
+      },
+      
+    ],
+  });
