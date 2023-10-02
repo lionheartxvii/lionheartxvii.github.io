83ca1de67b8a3ca22ab343732cabe23bcab27a6a
lionheartxvii <71563922+lionheartxvii@users.noreply.github.com>
2023-10-02 19:57:25 +0200

    parallax effect and scroll in view added

--- javascript/script.js
+++ javascript/script.js
@@ -362,3 +362,32 @@ function dropdownMenu(){
 // window.addEventListener("load", berechneBreiten);
 // window.addEventListener("resize", berechneBreiten);
 // window.addEventListener("DOMContentLoaded", berechneBreiten);
+
+
+
+
+
+
+// Quelle: Youtube: DesignCourse, "Awesome Scrolling SVG Line Drawing - How they did it"
+
+    window.addEventListener('scroll', () => {
+
+        const target = document.querySelectorAll('.scroll');
+
+
+        var index = 0,
+            length = target.length;
+        for (index; index < length; index++) {
+            var pos = window.pageYOffset * target[index].dataset.rate;
+
+            if (target[index].dataset.direction === 'vertical') {
+                target[index].style.transform = 'translate3d(0px, ' + pos + 'px, 0px)';
+            } else {
+                var posX = window.pageYOffset * target[index].dataset.ratex;
+                var posY = window.pageYOffset * target[index].dataset.ratey;
+
+                target[index].style.transform = 'translate3d(' + posX + 'px, ' + posY + 'px, 0 px)';
+            }
+        }
+    })
+
